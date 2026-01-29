46f48b603c7a08b6b5ea24cbdfe2cbbd9d3ce087
antony183 <antonyrodriguezsilvera@gmail.com>
2026-01-29 11:39:58 -0500

    Add Persona class with encapsulation and polymorphism

---
+++ servicios/gestor_personas.py
@@ -0,0 +1,19 @@
+# Clase base Persona
+# Aplica encapsulación y polimorfismo
+
+class Persona:
+    def __init__(self, nombre, edad):
+        # Atributos privados (encapsulación)
+        self.__nombre = nombre
+        self.__edad = edad
+
+    # Métodos getter
+    def get_nombre(self):
+        return self.__nombre
+
+    def get_edad(self):
+        return self.__edad
+
+    # Método polimórfico
+    def presentarse(self):
+        return f"Hola, soy {self.__nombre} y tengo {self.__edad} años."

764475217a7c42201ad638d1a4a45b84a860096e
antony183 <antonyrodriguezsilvera@gmail.com>
2026-01-29 11:37:49 -0500

    Add Estudiante class with inheritance from Persona
    Implement Estudiante class inheriting from Persona with overridden method.

---
+++ modelos/estudiante.py
@@ -0,0 +1,14 @@
+# Clase derivada Estudiante
+# Aplica herencia y polimorfismo
+
+from .persona import Persona   # 👈 IMPORT CORRECTO
+
+class Estudiante(Persona):
+    def __init__(self, nombre, edad, carrera):
+        # Llamada al constructor de la clase base
+        super().__init__(nombre, edad)
+        self.carrera = carrera
+
+    # Método sobrescrito (polimorfismo)
+    def presentarse(self):
+        return f"Hola, soy {self.get_nombre()}, estudio {self.carrera} y tengo {self.get_edad()} años."
